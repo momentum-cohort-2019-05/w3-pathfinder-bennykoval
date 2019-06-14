c9f1900018ba595931abeca7b5586d0f19dec9e1
bennykoval <bethanykoval@gmail.com>
2019-06-13 20:12:14 -0400

    Install numpy

--- pathfinder.py
+++ pathfinder.py
@@ -1,2 +1,17 @@
-from PIL import pillow
+#from PIL import pillow
+import numpy as np
 
+map_data = "elevation_small.txt"
+
+a = numpy.array([1,2,3,4])
+
+# with open(map_data) as f:
+#     map_data_list = []
+#     for line in f:
+#         map_data_list.append([int(x) for x in line.split()])
+#     #print(map_data_list)
+
+print(a.index(3))
+#numpy.loadtxt("elevation_small.txt", usecols=range(1,8))
+
+#print(map_data_list.index(4068))
